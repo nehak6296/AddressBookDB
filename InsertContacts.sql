8878187d3d0f2b6c9db2a68dc84ed275f9ea7a50
Neha <nehak6296@gmail.com>
2021-04-09 23:19:07 +0530

    [Neha]Added:Inserted Contacts into AddressBookTable

---
+++ InsertContacts.sql
@@ -0,0 +1,9 @@
+
+insert into AddressBook values
+('Neha','Kotarwar','Ravi Nagar','Karanja','Maharashta',444206,'7768965972','neha@gmail.com'),
+('Harish','Sharma','Gandhi Nagar','Amravati','Maharashta',442206,'9876543223','harish@gmail.com'),
+('Kajal','Mishra','Lotus Colony','Hyderabad','Telangana',432206,'9876543435','kajal@gmail.com'),
+('Sayali','Patil','Sai Nagar','Bhopal','MadhyaPradesh',342206,'8976543223','sayali@yahoo.com'),
+('Om','Kale','jk road','Shrinagar','JK',442206,'9871143223','harish@yahoo.com');
+
+
