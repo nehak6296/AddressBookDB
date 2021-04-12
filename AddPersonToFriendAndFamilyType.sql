54df33447b5ca306908829928d5b50dc69d8c141
Neha <nehak6296@gmail.com>
2021-04-12 19:28:15 +0530

    [Neha]Added:inserted Person to both Friend and family addressbook type

---
+++ AddPersonToFriendAndFamilyType.sql
@@ -0,0 +1,4 @@
+
+insert into AddressBook values
+('Maya','Kapoor','mg road','Jaipur','Rajastan',333653,'7788778877','mk@gmail.com','addressbook4','Friend'),
+('Maya','Kapoor','mg road','Jaipur','Rajastan',333653,'7788778877','mk@gmail.com','addressbook4','Family');
